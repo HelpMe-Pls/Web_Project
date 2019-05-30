f1ab15d0095fb247836be568472f23147ccef060
Hieunguyen <kerituni123@gmail.com>
2019-05-30 17:35:51 +0700

    len 2

--- profile.php
+++ profile.php
@@ -8,40 +8,7 @@
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
 
-	<div class="header-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="user-menu">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-heart"></i> Yêu thích</a></li>
-                            <li><a href="cart.html"><i class="fa fa-user"></i>Giỏ hàng</a></li>
-                            <li><a href="checkout.html"><i class="fa fa-user"></i> Thanh toán</a></li>
-                            <li><a href="login.php"><i class="fa fa-user"></i> Đăng nhập</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End header area -->
-    
-    <div class="site-branding-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="logo">
-                        <h1><img src="imgs/logo.png"></h1>
-                    </div>
-                </div>
-                
-                <div class="col-sm-6">
-                    <div class="shopping-item">
-                        <a href="cart.html">Giỏ hàng - <span class="cart-amunt">0</span><span <span style="color: #5a88ca; font-weight: 700;">đ</span><i class="fa fa-shopping-cart"></i> <span class="product-count">0</span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End site branding area -->
+	<?php include 'includes/navbar.php' ?>
     
     <div class="mainmenu-area">
         <div class="container">
